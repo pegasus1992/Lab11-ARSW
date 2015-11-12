49240aaf2984ef75ec63850063cdee82f40888d4
jean9108 <joana9108@gmail.com>
2015-11-12 11:47:16 -0500

    Dibujar

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -1,5 +1,59 @@
 (function () {
     var app = angular.module('modone', []);
+    app.controller('plan_control', function ($scope, $http, $log) {
+        $scope.arreglo = [];
+        $scope.seleccionado = "";
+
+        $scope.loadData = function () {
+            var configList = {
+                method: "GET",
+                url: "blueprints"
+            };
+
+            var response = $http(configList);
+
+            response.success(function (data, status, headers, config) {
+                $scope.arreglo = data;
+            });
+
+            response.error(function (data, status, headers, config) {
+                alert("The petition has failed. HTTP Status:" + status);
+            });
+        };
+        // $scope.loadData();
+
+        $scope.charge = function (blueprintname) {
+            $http({
+                method: 'GET',
+                url: '/blueprints/' + blueprintname
+            }).success(function (data) {
+                //console.log(data);
+                var cnv = document.getElementById("grafica");
+                var ctx = cnv.getContext("2d");
+                switch (blueprintname) {
+                    case 'rayon':
+                        ctx.beginPath();
+                        ctx.lineJoin = "round";
+                        ctx.moveTo(20, 20);
+                        ctx.lineTo(100, 50);
+                        ctx.lineTo(20, 100);
+                        ctx.stroke();
+                        break;
+                    case 'larecta':
+                        ctx.beginPath();
+                        ctx.lineJoin = "round";
+                        ctx.moveTo(20, 20);
+                        ctx.lineTo(500, 50);
+                        ctx.stroke();
+                        break;
+                    case 'poligono1':
+                        ctx.rect(20,30,145,124);
+                        ctx.stroke();
+                        break;
+                }
+            });
+        };
+    });
 })();
 
 
