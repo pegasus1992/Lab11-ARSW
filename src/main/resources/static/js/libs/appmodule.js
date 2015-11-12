2b016a4a64abcdb991c43c57504b1376ec6c5784
jean9108 <joana9108@gmail.com>
2015-11-12 12:42:36 -0500

    SVG Hecho

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -30,7 +30,8 @@
                 //console.log(data);
                 var cnv = document.getElementById("grafica");
                 var ctx = cnv.getContext("2d");
-                switch (blueprintname) {
+
+                switch (data.name) {
                     case 'rayon':
                         ctx.beginPath();
                         ctx.lineJoin = "round";
@@ -47,8 +48,43 @@
                         ctx.stroke();
                         break;
                     case 'poligono1':
-                        ctx.rect(20,30,145,124);
+                        ctx.rect(20, 30, 145, 124);
                         ctx.stroke();
+                        break;
+                }
+            });
+            
+        };
+        $scope.chargeSVG = function (blueprintname) {
+            $http({
+                method: 'GET',
+                url: '/blueprints/' + blueprintname
+            }).success(function (data) {
+                //console.log(data);
+                var svg0 = document.getElementById("graficaSVG");
+
+                switch (data.name) {
+                    case 'rayon':
+                        svg = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                        svg.setAttribute('points',"20,20 40, 25 60,40 80, 120 120,140 200,180");
+                        svg.setAttribute('stroke','black');
+                        svg0.appendChild(svg);
+                        break;
+                    case 'larecta':
+                        svg = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                        svg.setAttribute('x1',20);
+                        svg.setAttribute('y1',40);
+                        svg.setAttribute('x2',30);
+                        svg.setAttribute('y2',50);
+                        svg.setAttribute('stroke','black');
+                        svg0.appendChild(svg);
+                        alert("Entro");
+                        break;
+                    case 'poligono1':
+                        svg = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                        svg.setAttribute('points',"200,10 250,190 160,210");
+                        svg.setAttribute('stroke','black');
+                        svg0.appendChild(svg);
                         break;
                 }
             });
