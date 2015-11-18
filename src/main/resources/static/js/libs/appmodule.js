fb119e8fcb4d418243b3770eb0665f768074a1ea
pegasus1992 <iguanodontecazador@gmail.com>
2015-11-18 18:48:18 -0500

    mejorado dibujado en canvas (puntos proveidos por el modelo)

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -28,63 +28,48 @@
                 url: '/blueprints/' + blueprintname
             }).success(function (data) {
                 //console.log(data);
-                var cnv = document.getElementById("grafica");
-                var ctx = cnv.getContext("2d");
-
+                var canvas = document.getElementById("grafica");
+                var canvasContext = canvas.getContext("2d");
+                var svg = document.getElementById("graficaSVG");
+                var svgContext;
                 switch (data.name) {
                     case 'rayon':
-                        ctx.beginPath();
-                        ctx.lineJoin = "round";
-                        ctx.moveTo(20, 20);
-                        ctx.lineTo(100, 50);
-                        ctx.lineTo(20, 100);
-                        ctx.stroke();
+                        canvasContext.beginPath();
+                        canvasContext.lineJoin = "round";
+                        canvasContext.moveTo(data.points[0].x, data.points[0].y);
+                        for (var i = 1; i < data.points.length; i++) {
+                            canvasContext.lineTo(data.points[i].x, data.points[i].y);
+                        }
+                        canvasContext.stroke();
+                        svgContext = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                        svgContext.setAttribute('points', "20,20 40, 25 60,40 80, 120 120,140 200,180");
+                        svgContext.setAttribute('stroke', 'black');
+                        svg.appendChild(svgContext);
                         break;
                     case 'larecta':
-                        ctx.beginPath();
-                        ctx.lineJoin = "round";
-                        ctx.moveTo(20, 20);
-                        ctx.lineTo(500, 50);
-                        ctx.stroke();
+                        canvasContext.beginPath();
+                        canvasContext.lineJoin = "round";
+                        canvasContext.moveTo(data.points[0].x, data.points[0].y);
+                        canvasContext.lineTo(data.points[1].x, data.points[1].y);
+                        canvasContext.stroke();
+                        svgContext = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                        svgContext.setAttribute('x1', 20);
+                        svgContext.setAttribute('y1', 40);
+                        svgContext.setAttribute('x2', 30);
+                        svgContext.setAttribute('y2', 50);
+                        svgContext.setAttribute('stroke', 'black');
+                        svg.appendChild(svgContext);
                         break;
                     case 'poligono1':
-                        ctx.rect(20, 30, 145, 124);
-                        ctx.stroke();
-                        break;
-                }
-            });
-            
-        };
-        $scope.chargeSVG = function (blueprintname) {
-            $http({
-                method: 'GET',
-                url: '/blueprints/' + blueprintname
-            }).success(function (data) {
-                //console.log(data);
-                var svg0 = document.getElementById("graficaSVG");
-
-                switch (data.name) {
-                    case 'rayon':
-                        svg = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
-                        svg.setAttribute('points',"20,20 40, 25 60,40 80, 120 120,140 200,180");
-                        svg.setAttribute('stroke','black');
-                        svg0.appendChild(svg);
-                        break;
-                    case 'larecta':
-                        svg = document.createElementNS("http://www.w3.org/2000/svg", "line");
-                        svg.setAttribute('x1',20);
-                        svg.setAttribute('y1',40);
-                        svg.setAttribute('x2',30);
-                        svg.setAttribute('y2',50);
-                        svg.setAttribute('stroke','black');
-                        svg0.appendChild(svg);
-                        alert("Entro");
-                        break;
-                    case 'poligono1':
-                        svg = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
-                        svg.setAttribute('points',"200,10 250,190 160,210");
-                        svg.setAttribute('stroke','black');
-                        svg0.appendChild(svg);
+                        canvasContext.moveTo(data.points[0].x, data.points[0].y);
+                        for (var i = 1; i < data.points.length; i++) {
+                            canvasContext.lineTo(data.points[i].x, data.points[i].y);
+                        }
+                        canvasContext.stroke();
+                        svgContext = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                        svgContext.setAttribute('points', "200,10 250,190 160,210");
+                        svgContext.setAttribute('stroke', 'black');
+                        svg.appendChild(svgContext);
                         break;
                 }
             });
